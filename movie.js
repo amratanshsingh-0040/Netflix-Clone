bfa7bd8e66a86e068b835d4a9ceeaa8e5593d104
Amratansh Singh <amratanshsingh920@gmail.com>
2026-06-10 20:15:36 +0530

    Update TMDB API key in movie.js

--- movie.js
+++ movie.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_TMDB_API_KEY";
+const API_KEY = "db98ef37f98fdceb034eb3c3bdb6f6de";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
